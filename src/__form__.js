652fdd7b732de674faafcb46ecabcfc5e0ef540c
Abrar-0 <abrarzawad222@gmail.com>
2024-04-14 21:56:40 +0600

    starting

---
+++ src/__form__.js
@@ -0,0 +1,6 @@
+// use form tag and name to get values
+// ways to get input value
+// 1. use e -> e.target.name of the field.values
+// 2. use individual field state for each input field == controlled
+// 3. one object holding all the input field values == controlled
+// 4. useRef to create a reference to the element and access value -> nameRef.current.value == uncontrolled
---
+++ src/components/StatefulForm/StatefulForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const StatefulForm = () => {
+    
+    const[email, setEmail] = useState(null);
+    const[password, setPassword] = useState(null);
+    const[name, setName] = useState(null);
+    const[error,setError] = useState('')
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        if(password.length<6){
+            setError('Password should be longer')
+        }
+        else{
+            setError('')
+            console.log(name,email,password)
+        }
+    }
+
+    const handleEmail = e =>{
+        // console.log(e.target.value)
+        setEmail(e.target.value)
+    }
+    const handlePassword = e =>{
+        setPassword(e.target.value)
+    }
+    const handleName = e =>{
+        setName(e.target.value)
+    }
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input onChange={handleName} type="text" name="name" />
+          <br />
+          <input onChange={handleEmail} type="email" name="email" id="" />
+          <br />
+          <input onChange={handlePassword} type='password' name='password' required/>
+          <br />
+          <input type="submit" value="Submit" />
+          {
+            error && <p>{error}</p>
+          }
+        </form>
+      </div>
+    );
+};
+
+export default StatefulForm;
